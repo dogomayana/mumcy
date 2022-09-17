f506ca065cd7b9b558bfe0a83c3ff591b219a9e3
dogomayana <thomasstephen252@gmail.com>
2022-09-17 15:59:44 -0800

    v1.0

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,11 +7,14 @@ export default function Home() {
   return (
     <>
       <Hero/>
-    <div className='w-full mx-auto flex flex-wrap -my-4 py-2 rounded-t-2xl bg-white'>
-        {data.products.map((item, index)=>(
+    <div className='w-full mx-auto -mt-4 mb-20 py-2 rounded-t-3xl bg-white'>
+      <div className='md:w-11/12 flex flex-wrap mx-auto'>
 
+        {data.products.map((item, index)=>(
+          
         <FoodItems image={item.image} name={item.name} desc={item.desc} item={item} price={item.price} key={index}/>
         ))}
+        </div>
 
     </div>
     </>
